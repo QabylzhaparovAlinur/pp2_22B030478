822a03640b467e3780bcfff039603da1585d4c53
QabylzhaparovAlinur <alinurkabylzhaparov@gmail.com>
2023-01-28 22:51:39 +0600

    LIsts

---
+++ TSIS2/Lists/9CopyList.py
@@ -0,0 +1,8 @@
+# list2 = list1 doesn't work
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
